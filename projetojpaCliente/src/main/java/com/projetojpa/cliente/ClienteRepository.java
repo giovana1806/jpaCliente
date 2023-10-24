fa08a53ef9ad4358d9eb08e66a49b95402f265b3
giovana1806 <132360035+giovana1806@users.noreply.github.com>
2023-10-24 08:32:39 -0300

    Delete projetojpaCliente directory

--- projetojpaCliente/src/main/java/com/projetojpa/cliente/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projetojpa.cliente;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projetojpa.entities.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-
-}
